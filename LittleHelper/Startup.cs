1ea81795fae19f3f920aa6239ce68216dbbc4190
Daniel Rogers <boldanrogers@gmail.com>
2021-07-20 12:31:53 -0400

    Add project files.

---
+++ LittleHelper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LittleHelper.Startup))]
+namespace LittleHelper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
