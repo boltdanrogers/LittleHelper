7b92e3932eebfc17199fe5ba3c43ff701a5bee15
Daniel Rogers <boldanrogers@gmail.com>
2021-07-25 18:22:43 -0400

    enabled migrations, added a migration and then updated the database

---
+++ LittleHelper.Data/Migrations/202107252221084_initialMigration.cs
@@ -0,0 +1,179 @@
+namespace LittleHelper.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.FoodItem",
+                c => new
+                    {
+                        FoodId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Unit = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.FoodId);
+            
+            CreateTable(
+                "dbo.RecipeEntry",
+                c => new
+                    {
+                        RecipeId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Instructions = c.String(nullable: false),
+                        Cooked = c.Boolean(nullable: false),
+                        Cooled = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.RecipeId);
+            
+            CreateTable(
+                "dbo.RecipeFood",
+                c => new
+                    {
+                        FoodNeededId = c.Int(nullable: false, identity: true),
+                        RecipeId = c.Int(nullable: false),
+                        FoodId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Unit = c.String(),
+                    })
+                .PrimaryKey(t => t.FoodNeededId)
+                .ForeignKey("dbo.FoodItem", t => t.FoodId, cascadeDelete: true)
+                .ForeignKey("dbo.RecipeEntry", t => t.RecipeId, cascadeDelete: true)
+                .Index(t => t.RecipeId)
+                .Index(t => t.FoodId);
+            
+            CreateTable(
+                "dbo.IdentityRole",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserRole",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(),
+                        IdentityRole_Id = c.String(maxLength: 128),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.IdentityRole_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.StorageArea",
+                c => new
+                    {
+                        StrorageId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Size = c.Int(nullable: false),
+                        Powered = c.Boolean(nullable: false),
+                        Spaces = c.Int(nullable: false),
+                        SpaceNames = c.String(),
+                    })
+                .PrimaryKey(t => t.StrorageId);
+            
+            CreateTable(
+                "dbo.StorageFood",
+                c => new
+                    {
+                        FoodStoredId = c.Int(nullable: false, identity: true),
+                        StorageId = c.Int(nullable: false),
+                        FoodId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Unit = c.String(),
+                    })
+                .PrimaryKey(t => t.FoodStoredId)
+                .ForeignKey("dbo.FoodItem", t => t.FoodId, cascadeDelete: true)
+                .ForeignKey("dbo.StorageArea", t => t.StorageId, cascadeDelete: true)
+                .Index(t => t.StorageId)
+                .Index(t => t.FoodId);
+            
+            CreateTable(
+                "dbo.ApplicationUser",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserClaim",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserLogin",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.IdentityUserRole", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.StorageFood", "StorageId", "dbo.StorageArea");
+            DropForeignKey("dbo.StorageFood", "FoodId", "dbo.FoodItem");
+            DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.RecipeFood", "RecipeId", "dbo.RecipeEntry");
+            DropForeignKey("dbo.RecipeFood", "FoodId", "dbo.FoodItem");
+            DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.StorageFood", new[] { "FoodId" });
+            DropIndex("dbo.StorageFood", new[] { "StorageId" });
+            DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.RecipeFood", new[] { "FoodId" });
+            DropIndex("dbo.RecipeFood", new[] { "RecipeId" });
+            DropTable("dbo.IdentityUserLogin");
+            DropTable("dbo.IdentityUserClaim");
+            DropTable("dbo.ApplicationUser");
+            DropTable("dbo.StorageFood");
+            DropTable("dbo.StorageArea");
+            DropTable("dbo.IdentityUserRole");
+            DropTable("dbo.IdentityRole");
+            DropTable("dbo.RecipeFood");
+            DropTable("dbo.RecipeEntry");
+            DropTable("dbo.FoodItem");
+        }
+    }
+}
