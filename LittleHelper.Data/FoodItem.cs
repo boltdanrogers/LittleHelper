a00b5179b9dc6bdcb90d4b5fd9a5f6f148e5decc
Daniel Rogers <boldanrogers@gmail.com>
2021-07-24 20:21:07 -0400

    made needed changes to where the identityModels was and added references etc, changes to account controller

---
+++ LittleHelper.Data/FoodItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleHelper.Data
+{
+    public class FoodItem
+    {
+        [Key]
+        public int FoodId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Unit { get; set; }
+
+
+    }//end of class FoodItem
+}//end of name space
