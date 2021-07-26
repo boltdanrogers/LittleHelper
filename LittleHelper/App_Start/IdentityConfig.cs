feba54939ebc4f7dc120c6be1c161c2923e0e3c2
Daniel Rogers <boldanrogers@gmail.com>
2021-07-25 22:48:53 -0400

    changed password requirements in web.config

--- LittleHelper/App_Start/IdentityConfig.cs
+++ LittleHelper/App_Start/IdentityConfig.cs
@@ -55,10 +55,10 @@ namespace LittleHelper
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,//true,
+                RequireDigit = false,//true,
+                RequireLowercase = false,//true,
+                RequireUppercase = false,//true,
             };
 
             // Configure user lockout defaults
