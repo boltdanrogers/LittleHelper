e2c23cd6bc4ab46453e505aa595468136706c4d6
Daniel Rogers <boldanrogers@gmail.com>
2021-07-27 22:06:03 -0400

    created a foodItem service class and added references . added foodItemController

---
+++ LittleHelper.Services/FoodItemService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleHelper.Services
+{
+    public class FoodItemService
+    {
+        //this is for our crud, to interact with the database using different models
+
+        //create with FoodItemCreate model
+
+        //get all FoodItems  with FoodItemListItem
+
+        //get one particular 
+
+
+
+    }//end of class FoodItemService
+}//end of name space
---
+++ LittleHelper/Controllers/FoodItemController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LittleHelper.Controllers
+{
+    //need to add the authorize annotation 
+    [Authorize]
+    //take away or comment out if un logged in users can see the page. might be useful to see about having the index not require that, but the create page need it.... hmmmm wait those are all here. quick search shows the annotation
+
+    public class FoodItemController : Controller
+    {
+
+        //start with the private method to create the instance of the FoodItem service
+
+
+
+
+        // GET: FoodItem
+        public ActionResult Index()
+        {
+            return View();
+        }//end of method Index
+
+
+
+
+
+
+
+
+
+
+
+    }//end of class FoodItemController
+}//end of name space
