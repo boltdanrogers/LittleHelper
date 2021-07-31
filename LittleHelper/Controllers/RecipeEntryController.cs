33e57fce41ae82e80eca7b1ec332a92bf7948801
Daniel Rogers <boldanrogers@gmail.com>
2021-07-31 01:05:25 -0400

    finishedstorageAreaController, mocked up the get methods on the recipeEntry controller

--- LittleHelper/Controllers/RecipeEntryController.cs
+++ LittleHelper/Controllers/RecipeEntryController.cs
@@ -11,14 +11,37 @@ namespace LittleHelper.Controllers
     {
 
         //if possible make this method not need authorization
-        // GET: RecipeEntry
+        // get all
         public ActionResult Index()
         {
             return View();
         }//end of method Index
 
+        //get create
+        public ActionResult Create()
+        {
+            return View();
+        }//end of method
+        //post create
 
+        //get single details
+        public ActionResult Details()
+        {
+            return View();
+        }//end of method
+        //get edit
+        public ActionResult Edit()
+        {
+            return View();
+        }//end of method
+        //post edit
 
+        //get delete
+        public ActionResult Delete()
+        {
+            return View();
+        }//end of method
+        //post delete
 
     }//end of class RecipeEntryController
 }//end of name space
--- LittleHelper/Controllers/StorageAreaController.cs
+++ LittleHelper/Controllers/StorageAreaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LittleHelper.Models.StorageArea;
+using LittleHelper.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +10,114 @@ namespace LittleHelper.Controllers
 {
     public class StorageAreaController : Controller
     {
-        // GET: StorageArea
+        // get all
         public ActionResult Index()
         {
-            return View();
+            var service = new StorageAreaService();
+            var model = service.GetStorageItems();
+
+            return View(model);
+
         }//end of Index method
-    
-    
-    
-    
-    
+
+        //get create
+        public ActionResult Create()
+        {
+
+            return View();
+        }//end of get create
+
+        //post create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(StorageAreaCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var service = new StorageAreaService();
+            if (service.CreateStorageArea(model))
+            {
+                TempData["SaveResult"] = "Your storage area was created";
+                return RedirectToAction("index");
+
+            }//end of if created the storage area
+            ModelState.AddModelError("", "Storage was not created");
+            return View(model);
+
+        }//end of post create
+
+
+        //get single details
+        public ActionResult Details(int id)
+        {
+            var service = new StorageAreaService();
+            var model = service.GetStorageAreaById(id);
+            return View(model);
+
+        }//end of method get details
+
+        //get edit
+        public ActionResult Edit(int id)
+        {
+            var service = new StorageAreaService();
+            var detail = service.GetStorageAreaById(id);
+            var model = new StorageAreaEdit { StrorageId = detail.StorageId, Name = detail.Name, Size = detail.Size, Powered = detail.Powered, Spaces = detail.Spaces, SpaceNames = detail.SpaceNames };
+            return View(model);
+
+        }//end of get edit
+
+        //post edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, StorageAreaEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.StrorageId != id)
+            {
+                ModelState.AddModelError("", "ID mismatch");
+                return View(model);
+            }//end of if id mismatch
+
+            var service = new StorageAreaService();
+            
+            if (service.UpdateStorageArea(model))
+            {
+                TempData["SaveResult"] = "Your storage area was updated";
+                return RedirectToAction("index");
+
+            }//end of if created the storage area
+            ModelState.AddModelError("", "Storage was not updated");
+            return View(model);
+
+        }//end of post edit
+
+
+        //get delete
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var service = new FoodItemService();
+            var model = service.GetFoodItemById(id);
+            return View(model);
+
+        }//end of get method delete
+
+
+
+        //post delete
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteStorage(int id)
+        {
+            var service = new StorageAreaService();
+            service.DeleteStorageArea(id);
+
+            TempData["SaveResult"] = "Your storage was deleted";
+            return RedirectToAction("index");
+
+        }//end of method deleteStorage
+
+
     }//end of class StorageAreaController
 }//end of name space
